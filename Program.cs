ba82ba1a743fc992c9bcce77d6e6b06596dec751
Beat94 <36741096+Beat94@users.noreply.github.com>
2022-11-12 20:19:06 +0100

    Initalizing workerfiles

---
+++ Program.cs
@@ -0,0 +1,10 @@
+using downloadViewerWorker;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
