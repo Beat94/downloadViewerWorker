0dbdcb82752dbdbf4665b059e754584d041f709b
Beat94 <36741096+Beat94@users.noreply.github.com>
2023-01-15 17:11:08 +0100

    updated ConfigElement

--- ConfigElement.cs
+++ ConfigElement.cs
@@ -5,6 +5,6 @@ namespace ViewerNamespace;
 public class ConfigElement
 {
     public string savelocation{get; set;}
-    public IList<string> ending{get; set;}
+    public IList<string>? ending{get; set;}
     
 }
