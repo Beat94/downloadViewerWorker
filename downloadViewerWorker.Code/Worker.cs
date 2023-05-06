18ceda7f3d17018d312c4dcbbc622e10eaa3b653
Beat94 <36741096+Beat94@users.noreply.github.com>
2023-05-06 12:26:08 +0200

    Beat94/cleanup (#18)
    * fixed 16 cleanup
    
    * Changed to sln and project folders

---
+++ downloadViewerWorker.Code/Worker.cs
@@ -0,0 +1,43 @@
+using Microsoft.Extensions.Logging;
+using ViewerNamespace;
+
+namespace downloadViewerWorker;
+
+public class Worker : BackgroundService
+{
+    private readonly ILogger<Worker> _logger;
+
+    public Worker(ILogger<Worker> logger)
+    {
+        _logger = logger;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        ConfigLoader cl = new ConfigLoader("./config.json");
+        ConfigImportTester cit = new ConfigImportTester(cl);
+        if(cl.error){
+            _logger.LogError(cl.errorMsg);
+        }
+
+        Viewer v = new Viewer("");
+
+        _logger.LogInformation(v.getFolder());
+        _logger.LogInformation(v.countFiles().ToString());
+
+        foreach(FileInfo file in v.fileInfo)
+        {
+            _logger.LogInformation(file.ToString());
+        }
+
+        _logger.LogInformation(cit.showConfig());
+
+        /*
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+            await Task.Delay(1000, stoppingToken);
+        }
+        */
+    }
+}
