01cd93bdcd627b4bc97dedd866fbfdeec9b0839e
Beat Giuliano Rey <beat_g_94@hotmail.com>
2023-05-21 15:12:00 +0200

    Adding mover-class

---
+++ downloadViewerWorker.Code/Mover.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace downloadViewerWorker.Code
+{
+    public class Mover
+    {
+    }
+}
